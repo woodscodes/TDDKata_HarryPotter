9a1b4d6df27aff70b30f8802a0c1d3794fbf291b
stuwo <stooberto@gmail.com>
2020-07-15 11:00:18 +0900

    basket tests #1

---
+++ TDDKata_HarryPotter.Core/Models/Basket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDKata_HarryPotter.Core.Models
+{
+    public class Basket
+    {
+        public List<Book> Items { get; set; }
+    }
+}
---
+++ TDDKata_HarryPotter.Tests/BasketTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDKata_HarryPotter.Core.Models;
+using Xunit;
+
+namespace TDDKata_HarryPotter.Tests
+{
+    public class BasketTests
+    {
+        [Fact]
+        public void ShouldAddABookToBasket()
+        {
+            var basket = new Basket();
+
+            Assert.IsType<Basket>(basket);
+        }
+
+        [Fact]
+        public void ShouldProvideANewList()
+        {
+            var basket = new Basket();
+        }
+
+        [Fact]
+        public void ShouldAddABook()
+        {
+            var basket = new Basket();
+            var book = new Book();
+            var expectedNumberOfBooks = 1;
+
+            basket.Items.Add(book);
+
+            Assert.Equal(basket.Items.Count(), expectedNumberOfBooks);
+        }
+    }
+}
