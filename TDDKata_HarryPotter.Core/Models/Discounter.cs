5a67fdc7b7249f02f8614677cec1e23db6794140
stuwo <stooberto@gmail.com>
2020-07-16 10:52:58 +0900

    discount tests (1 set of 1-5 unique titles) #2

--- TDDKata_HarryPotter.Core/Models/Discounter.cs
+++ TDDKata_HarryPotter.Core/Models/Discounter.cs
@@ -12,13 +12,41 @@ namespace TDDKata_HarryPotter.Core.Models
 
         public static decimal CalculateTotal(Basket basket)
         {
+            var uniqueIsbnCounter = 0;
+
             for (int i = 0; i < basket.Items.Count() - 1; i++)
             {
                 if (basket.Items[i].Isbn != basket.Items[i + 1].Isbn)
-                    _discountApplied = 5m;
+                {
+                    uniqueIsbnCounter++;
+                }
             }
 
-            return basket.Total-(basket.Total*_discountApplied/100);
+            return basket.Total-(basket.Total*getDiscount(uniqueIsbnCounter)/100);
+        }
+
+        private static decimal getDiscount(int uniqueIsbnCounter)
+        {
+            switch (uniqueIsbnCounter)
+            {
+                case 1:
+                    _discountApplied = 5m;
+                    break;
+                case 2:
+                    _discountApplied = 10m;
+                    break;
+                case 3:
+                    _discountApplied = 20m;
+                    break;
+                case 4:
+                    _discountApplied = 25m;
+                    break;
+                default:
+                    _discountApplied = 0m;
+                    break;
+            }
+
+            return _discountApplied;
         }
     }
 }
