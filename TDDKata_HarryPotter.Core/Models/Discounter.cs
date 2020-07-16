62f82c7489604b6ad99513f86b45b453ee2f1715
stuwo <stooberto@gmail.com>
2020-07-16 10:31:54 +0900

    discount tests (accessing and two unique books) #1

---
+++ TDDKata_HarryPotter.Core/Models/Discounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDKata_HarryPotter.Core.Models
+{
+    public static class Discounter
+    {
+        private static decimal _discountApplied;
+
+        public static decimal CalculateTotal(Basket basket)
+        {
+            for (int i = 0; i < basket.Items.Count() - 1; i++)
+            {
+                if (basket.Items[i].Isbn != basket.Items[i + 1].Isbn)
+                    _discountApplied = 5m;
+            }
+
+            return basket.Total-(basket.Total*_discountApplied/100);
+        }
+    }
+}
--- TDDKata_HarryPotter.Tests/DiscountTests.cs
+++ TDDKata_HarryPotter.Tests/DiscountTests.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TDDKata_HarryPotter.Core.Models;
 using Xunit;
 
 namespace TDDKata_HarryPotter.Tests
 {
     public class DiscountTests
     {
+        [Fact]
+        public void ShouldAccessTheDiscounter()
+        {           
+            var expectedTotal = 0m;
+            var basket = new Basket();
 
+            Assert.Equal(Discounter.CalculateTotal(basket), expectedTotal);
+        }
+
+        [Fact]
+        public void ShouldCalculateDiscountForTwoUniqueBooks()
+        {
+            var basket = new Basket();
+            basket.Items.Add(new Book { Isbn = "0-7475-3269-9", Price = 8.0m, Title = "Philosopher's Stone" });
+            basket.Items.Add(new Book { Isbn = "0-7475-3849-2", Price = 8.0m, Title = "Chamber of Secrets" });
+            var expectedTotal = 15.2m;
+
+            var total = Discounter.CalculateTotal(basket);
+
+            Assert.Equal(total, expectedTotal);
+        }
     }
 }
