b36e4d6f1485f12a4ddd9600474e9d72e9266b12
stuwo <stooberto@gmail.com>
2020-07-17 10:03:36 +0900

    book tests

--- TDDKata_HarryPotter.Tests/BookTests.cs
+++ TDDKata_HarryPotter.Tests/BookTests.cs
@@ -5,10 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using TDDKata_HarryPotter.Core.Models;
 using Xunit;
+using Xunit.Sdk;
 
 namespace TDDKata_HarryPotter.Tests
 {
     public class BookTests
     {
+        [Fact]
+        public void ShouldCreateBook()
+        {
+            var book = new Book { Volume = 1, Price = 8.0m };
+
+            Assert.IsType<Book>(book);
+        }
     }
 }
