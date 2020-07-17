481985625a3758ad0eeb3ed40bf5a4764b1dda16
stuwo <stooberto@gmail.com>
2020-07-17 10:07:58 +0900

    basket tests

--- TDDKata_HarryPotter.Tests/BasketTests.cs
+++ TDDKata_HarryPotter.Tests/BasketTests.cs
@@ -10,6 +10,16 @@ namespace TDDKata_HarryPotter.Tests
 {
     public class BasketTests
     {
+        [Fact]
+        public void ShouldAddBooksToBasket()
+        {
+            var basket = new Basket();
+            basket.Items.Add(new Book { Volume = 1, Price = 8.0m });
+            basket.Items.Add(new Book { Volume = 1, Price = 8.0m });
+            var expectedBasketCount = 2;
+
+            Assert.Equal(basket.Items.Count, expectedBasketCount);           
+        }
 
     }
 }
