d8d539cbd61cfec5131c54eb66001e28c0339a40
stuwo <stooberto@gmail.com>
2020-07-24 09:30:24 +0900

    added new set of tests (set manager) to try to fix the optimisation problem

---
+++ TDDKata_HarryPotter.Core/Models/SetManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDKata_HarryPotter.Core.Models
+{
+    public class SetManager
+    {
+        private List<HashSet<int>> _discountCombinations = new List<HashSet<int>>();
+
+        public SetManager(List<int> books)
+        {
+            BuildSets(books);
+        }
+
+        public void BuildSets(List<int> books)
+        {
+            foreach (var book in books)
+            {
+                if (!_discountCombinations.Any(d => d.Add(book)))
+                    _discountCombinations.Add(new HashSet<int> { book });
+            }
+        }
+
+        public int Count()
+        {
+            return _discountCombinations.Count();
+        }
+    }
+}
---
+++ TDDKata_HarryPotter.Tests/SetTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDKata_HarryPotter.Core.Models;
+using Xunit;
+
+namespace TDDKata_HarryPotter.Tests
+{
+    public class SetTests
+    {
+        [Fact]
+        public void ShouldCreate2SetsOf1()
+        {
+            var books = new List<int> { 1, 1 };
+            var sets = new SetManager(books);
+           
+            Assert.Equal(2, sets.Count());
+        }
+        
+        [Fact]
+        public void ShouldCreate1SetOf2()
+        {
+            var books = new List<int> { 1, 2 };
+            var sets = new SetManager(books);
+
+            Assert.Equal(1, sets.Count());
+        }
+    }
+}
